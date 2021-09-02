06339d6b7a40b8318c485f23fa49dc932151eab2
Behzad Chaharbaghi <82478699+BehzadChaharbaghi@users.noreply.github.com>
2021-09-02 12:51:22 +0430

    add-home

---
+++ src/pages/home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../../components/header/Header';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>صفحه خانه</h1>
+        </div>
+    );
+};
+
+export default Home;
