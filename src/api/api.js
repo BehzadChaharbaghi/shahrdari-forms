02e0cf7a8ae4d548f645113c42b7404b0553f36f
Behzad Chaharbaghi <82478699+BehzadChaharbaghi@users.noreply.github.com>
2021-09-02 12:55:37 +0430

    modified-api

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,7 @@ import Axios from "axios";
 export const getAxiosInstanceApi = () => {
     const token = localStorage.getItem("token");
     return Axios.create({
-        baseURL: `${token}`,
+        baseURL: "http://localhost:19749/api/",
         headers: {
             "Content-type": "application/json;",
             Authorization: "Bearer " + token,
