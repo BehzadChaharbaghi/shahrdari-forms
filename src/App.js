e4218fdba107de1a63d765c8c838c058b3d9e91c
Behzad Chaharbaghi <82478699+BehzadChaharbaghi@users.noreply.github.com>
2021-09-02 12:56:36 +0430

    modified-app

--- src/App.js
+++ src/App.js
@@ -7,18 +7,25 @@ import { AuthProvider } from './context/AuthContext';
 import VerifyTwoFactor from './pages/verify-two-factor/VerifyTwoFactor';
 import Register from './pages/register/Register';
 import Login from './pages/login/Login';
+import ResetPassword from './pages/reset-password/ResetPassword';
+import Layout from './components/layout/Layout';
+import Home from './pages/home/Home';
 
 const App = () => {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path={"/Register"} component={Register} />
-          <Route exact path={"/Login"} component={Login} />
-          <Route path={"/VerifyTwoFactor"} component={VerifyTwoFactor} />
-        </Switch>
-      </BrowserRouter>
-    </AuthProvider>
+    <BrowserRouter>
+      <Switch>
+        <AuthProvider>
+          <Layout>
+            <Route exact path={"/"} component={Home} />
+            <Route path={"/Register"} component={Register} />
+            <Route path={"/Login"} component={Login} />
+            <Route path={"/VerifyTwoFactor"} component={VerifyTwoFactor} />
+            <Route path={"/ResetPassword"} component={ResetPassword} />
+          </Layout>
+        </AuthProvider>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
